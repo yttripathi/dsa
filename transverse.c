ed93e2e3c28a6d81abbaf3f43135e872da2d9304
yttripathi <tripathiyashasvi3@gmail.com>
2025-10-15 10:12:50 +0530

    new commit

---
+++ transverse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};  
+    int n = 5;  
+
+    printf("Array elements are:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);  
+    }
+
+    return 0;
+}
